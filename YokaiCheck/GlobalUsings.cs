3d1cdabb5a439c0649ee25e75644c28b135d471a
Haselnussbomber <mail@haselnussbomber.de>
2025-11-11 10:20:54 +0100

    Remove shared csproj, update HaselCommon

--- YokaiCheck/GlobalUsings.cs
+++ YokaiCheck/GlobalUsings.cs
@@ -3,4 +3,5 @@ global using AutoCtor;
 global using Dalamud.Bindings.ImGui;
 global using HaselCommon.Extensions;
 global using HaselCommon.Services;
+global using HaselCommon.Windows;
 global using Injectio.Attributes;
